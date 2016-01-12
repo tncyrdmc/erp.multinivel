bcbf06de2624ab4aeeb303fc4f6522058288d805
Cristian Vargas <cmvargase@hotmail.com>
2016-01-12 18:23:31 -0500

    correcciones carrito de compras

--- public_html/application/views/website/bo/comercial/altas/modificar_servicio.php
+++ public_html/application/views/website/bo/comercial/altas/modificar_servicio.php
@@ -178,11 +178,8 @@ $valor_total_publico=0;
 															<input type="number" min="1" max="" value='<?=$mercancia[0]->puntos_comisionables?>' name="puntos_com" id="puntos_com">
 														</label>
 													</section>
-													<?$i=0?>
-													<?foreach($impuestos_merc as $merc)
-													{?>	
 													<legend>Impuesto</legend>
-																<section class="col col-12" style="width: 50%;">País de la mercancía
+													<section class="col col-12" style="width: 50%;">País de la mercancía
 														<label class="select">
 															<select id="pais2" required name="pais" onChange="ImpuestosPais()">
 																<?foreach ($pais as $key)
@@ -199,9 +196,14 @@ $valor_total_publico=0;
 															</select>
 														</label>
 													</section>
-
-														<section class="col col-6" id="impuesto">Impuesto
-															<label class="select">
+													<?$i=0?>
+													<?foreach($impuestos_merc as $merc)
+													{?>	
+														<section id="impuesto">
+														<section class="col col-6"  id="<?= $i=$i+1?>" >Impuesto
+														<label class="select">
+															<select name="id_impuesto[]" onclick="Resultado_ConSin_iva('real','real_iva'); Resultado_ConSin_iva('costo','distribuidores_iva'); Resultado_ConSin_iva('costo_publico','publico_iva');">
+															
 																
 																	<?foreach ($impuesto as $key){
 																		if($key->id_pais==$mercancia[0]->pais){
@@ -221,7 +223,7 @@ $valor_total_publico=0;
 																		}
 
 																			?>
-																		<select name="id_impuesto[]" onclick="Resultado_ConSin_iva('real','real_iva'); Resultado_ConSin_iva('costo','distribuidores_iva'); Resultado_ConSin_iva('costo_publico','publico_iva');">
+																	
 																		<?if($merc->id_impuesto==$key->id_impuesto)
 																		{?>
 																			<option selected value='<?php echo $key->id_impuesto?>'>
@@ -235,13 +237,21 @@ $valor_total_publico=0;
 																			</option>
 																		<?}?>
 																	
-																</select>
-																<a class='txt-color-red' onclick="dell_impuesto(<?=$i?>)" style='cursor: pointer;'>Eliminar <i class="fa fa-minus"></i></a>
-														<?}}?>	</label>
+																
+																
+														<?}}?>	
+														
+														</select>
+														<a class='txt-color-red' onclick="dell_impuesto(<?=$i?>)" style='cursor: pointer;'>Eliminar <i class="fa fa-minus"></i></a>
+														</label>
 														</section>
-
-
+														</section>
+															
+															
 													<?}?>
+													<fieldset>
+													<div class="row">
+																	
 																					<section class="col col-6">Requiere especificación
 																<div class="inline-group">
 																	<label class="radio">
@@ -252,8 +262,17 @@ $valor_total_publico=0;
 																			<i></i>más IVA</label>
 																		</div>
 																	</section>
+																	<section class="col col-6" style="width: 50%">
+													<br>
+													<br>
+														<a onclick="add_impuesto()" style='cursor: pointer;'>Agregar impuesto<i class="fa fa-plus"></i></a>
+													</section>
+													
 																	</div>
+																	
+
 																	<div class="row">
+																	
 																						<section class="col col-6">
 														<label class="input">
 															Costo real con IVA
@@ -266,19 +285,18 @@ $valor_total_publico=0;
 															<input type="text" value="<? echo $valor_total_distribuidores; ?>" min="1" max="" name="distribuidores_iva" id="distribuidores_iva" disabled>
 														</label>
 													</section>
+													
 													</div>
 													<div class="row">
+													
 													<section class="col col-6">
 														<label class="input">
 															Costo público con IVA
 															<input type="text" value="<? echo $valor_total_publico; ?>" min="1" max="" name="publico_iva" id="publico_iva" disabled>
 														</label>
 													</section>
-													<section class="col col-6" style="width: 50%">
-													<br>
-													<br>
-														<a onclick="add_impuesto()" style='cursor: pointer;'>Agregar impuesto<i class="fa fa-plus"></i></a>
-													</section>
+													</fieldset>
+													</div>
 												</fieldset>
 											</div>
 										</div>
@@ -337,7 +355,8 @@ $(document).ready(function() {
 
 function add_impuesto()
 {
-	var code=	'<div id="'+i+'"><section class="col col-3" id="impuesto">Impuesto'
+	i=i+1;
+	var code=	'<div id="'+(i)+'"><section class="col col-3" id="impuesto">Impuesto'
 	+'<label class="select">'
 	+'<select name="id_impuesto[]">'
 	<?foreach ($impuesto as $key)
