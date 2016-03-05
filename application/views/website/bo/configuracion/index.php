79878a24ebc65ef0a4a5d6db4092d4f8adf61289
qcmarcel <qc.marcel@gmail.com>
2016-03-05 15:53:37 -0500

    boton email-dep bo config

--- application/views/website/bo/configuracion/index.php
+++ application/views/website/bo/configuracion/index.php
@@ -98,7 +98,7 @@
 													</a>
 												</div>
 											<div class="col-xs-10 col-sm-4 col-md-3 col-lg-2 link">
-													<a href="/bo/administracion/emails_departamentos">
+													<a href="/bo/configuracion/emails_departamentos">
 														<div class="minh well well-sm txt-color-white text-center link_dashboard" style="background:<?=$style[0]->btn_2_color?>">
 															<i class="fa fa-envelope fa-3x"></i>
 															<h1>E-mails</h1>
