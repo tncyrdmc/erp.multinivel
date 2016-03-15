494bc5ad494e304ef136ecb688fd3562694c9491
qcmarcel <qc.marcel@gmail.com>
2016-03-15 11:42:52 -0500

    creando entorno backup

---
+++ bk/bk_daily.sh
@@ -0,0 +1,5 @@
+#echo "dentro de script:";
+#urlbase="/home/content/57/11569157/html";
+urlbase="/var/www";
+project="/erp.multinivel";
+db_config=$urlbase$project"/application/config/database.php"; 
